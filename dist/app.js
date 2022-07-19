dc88392d4e62d0623a434e625267492d1965d9bb
facuerrobidart <85039144+facuerrobidart@users.noreply.github.com>
2022-07-18 23:35:04 -0300

    reddit post fetching, twiiter keys

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
+const reddit_snooper_1 = __importDefault(require("reddit-snooper"));
+const reddit_simple_1 = require("@ipmanlk/reddit-simple");
 dotenv_1.default.config();
-console.log("Client id is: ", process.env.REDDIT_CLIENT_ID);
+var snooper = new reddit_snooper_1.default({
+    app_id: process.env.REDDIT_CLIENT_ID,
+    api_secret: process.env.REDDIT_CLIENT_SECRET,
+    automatic_retries: true,
+    api_requests_per_minute: 20
+});
+reddit_simple_1.RedditSimple.RandomPost("yerbamate").then(res => console.log(res[0]));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import dotenv from 'dotenv';
+import Snooper from 'reddit-snooper';
+import { RedditSimple } from '@ipmanlk/reddit-simple';
+
+dotenv.config();
+
+var snooper = new Snooper({
+    app_id: process.env.REDDIT_CLIENT_ID,
+    api_secret: process.env.REDDIT_CLIENT_SECRET,
+    automatic_retries: true,
+    api_requests_per_minute: 20
+});
 
 
-dotenv.config()
-
+RedditSimple.RandomPost("yerbamate").then(res => console.log(res[0]));
